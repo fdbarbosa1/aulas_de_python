09a7d78fa089181d5985cd321b7f788b275d4b2b
fdbarbosa1 <fdbarbosa18@gmail.com>
2025-02-24 23:53:11 -0300

    exercicio while

--- aula7.py
+++ aula7.py
@@ -1,2 +1,21 @@
-nome = input("Digite seu nome  ")
-idade = input("Digite sua idade  ")
+"""
+Iterando strings com while
+"""
+#       012345678910
+# nome = 'Luiz Otávio'  # Iteráveis
+#      1110987654321
+
+
+nome = 'Maria Helena'  # Iteráveis
+
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    
+    novo_nome += f'*{letra}'
+    print (novo_nome)
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
