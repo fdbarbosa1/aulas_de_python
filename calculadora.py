722f61793979cc70f63b6b856a32f3c24a5ad309
fdbarbosa1 <fdbarbosa18@gmail.com>
2025-02-26 14:34:22 -0300

    calculadora arrumada

--- calculadora.py
+++ calculadora.py
@@ -27,19 +27,22 @@ while True:
         print('Digite apenas um operador.')
         continue
 
+    print ("Realizando sua conta. Confira o resultado abaixo: ")
 
-    ###
-
-
-
-
-
-
-
-
-
+    if operador == '+':
+        print(f'{num_1_float} + {num_2_float}=', num_1_float + num_2_float)
+    elif operador == '-':
+        print(f'{num_1_float} - {num_2_float}=', num_1_float - num_2_float)     
+    elif operador == '/':
+        print(f'{num_1_float} / {num_2_float}=', num_1_float / num_2_float)
+    elif operador == '*':
+        print(f'{num_1_float} * {num_2_float}=', num_1_float * num_2_float)
+    else:
+       print ("nunca deveria ter chegado aqui")
     
-
+   
+   
+   
     sair = input('Quer sair? [s]im: ').lower().startswith('s')
 
     if sair is True:
