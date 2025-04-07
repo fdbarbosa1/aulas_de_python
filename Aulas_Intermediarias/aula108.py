3b81a9709dc888c4241f9a8353d703eee0fdaef4
fdbarbosa1 <fdbarbosa18@gmail.com>
2025-04-07 02:53:56 -0300

    global é uma pratica ruim ?

--- Aulas_Intermediarias/aula108.py
+++ Aulas_Intermediarias/aula108.py
@@ -11,11 +11,11 @@ x = 1
 
 
 def escopo():
-    global x
+   # global x
     x = 10
 
     def outra_funcao():
-        global x
+    #    global x
         x = 11
         y = 2
         print(x, y)
